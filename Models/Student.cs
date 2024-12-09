7c372115b4340ed90286b992f98853f540b93686
sem <semdronten@gmail.com>
2024-12-09 10:46:43 +0100

    Student Model en data laten zien op home page

---
+++ Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LetsConnect.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? UserId { get; set; }
+
+        [Required]
+        public int StudentNumber { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string? inserts { get; set; } //Tussenvoegsel
+
+        [Required]
+        public string  Lastname { get; set; }
+
+        [Required]
+        public string StudentClass { get; set; }
+
+        [Required]
+        public int StundentWorkshop1 { get; set; }
+
+        [Required]
+        public int StundentWorkshop2 { get; set; }
+
+        [Required]
+        public int StundentWorkshop3 { get; set; }
+
+        [Required]
+        public bool HasMidDay { get; set; } = false; //check om te kijken of student een workshop heeft in de middag
+    }
+}
